29b169256286bb8e1f6822214683892ebbaeae0c
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2023-06-03 14:01:11 +0900

    Adjust schema and data store

--- app/schema.ts
+++ app/schema.ts
@@ -1,30 +1,78 @@
 import { z } from 'zod';
 
+/**
+ * Custom Zod schema to create a string-like with a unique fallback. See documentation
+ * below to coerce empty strings to `undefined` so we can always set the default value.
+ *
+ * {@link https://github.com/colinhacks/zod/issues/2070}
+ * @returns Composable/reusable Zod type to create a string with a unique fallback.
+ */
+const stringWithDefaultValue = (defaultValue?: string) => {
+  return z
+    .string()
+    .nullish()
+    .transform((x) => x ?? undefined)
+    .default(defaultValue ?? '');
+};
+
+/**
+ * Ensures that a string is a string compatible with the date. Falls back to
+ * an empty string if its not.
+ *
+ * @returns String that is compatible as date.
+ */
+const stringAsCompatibleDate = () => {
+  return z.preprocess((x) => {
+    if (typeof x !== 'string') {
+      return '';
+    }
+
+    // Make sure that the current string is a valid timestamp, else just use a default empty string.
+    const validTimestamp = new Date(Number.parseInt(x, 10)).getTime() > 0;
+    if (validTimestamp) {
+      return x;
+    }
+
+    return '';
+  }, stringWithDefaultValue());
+};
+
+/**
+ * Ensures that a string is a string that's compatible with the boolean literals `true` and
+ * `false`, and falls back to `false` if its not.
+ *
+ * @returns String that is compatible with boolean literals.
+ */
+const stringAsCompatibleBoolean = () => {
+  return z.preprocess((x) => {
+    if (!x) {
+      return 'false';
+    }
+
+    if (x !== 'false' && x !== 'true') {
+      return 'false';
+    }
+
+    return x;
+  }, z.union([z.literal('true'), z.literal('false')]));
+};
+
 export const storageKey = Object.freeze({
+  CONFIG_FROZEN_KEY: 'frozen',
   TITLE_STORAGE_KEY: 'title',
   CONTENT_STORAGE_KEY: 'content',
   LAST_UPDATED_STORAGE_KEY: 'last-updated',
 });
 
-export const notesSchema = z.object({
+export const dataSchema = z.object({
+  config: z.object({
+    frozen: stringAsCompatibleBoolean(),
+  }),
   notes: z.object({
-    // Coerce to `undefined` so we can always set the default value: https://github.com/colinhacks/zod/issues/2070.
-    title: z
-      .string()
-      .nullish()
-      .transform((x) => x ?? undefined)
-      .default(''),
-    content: z
-      .string()
-      .nullish()
-      .transform((x) => x ?? undefined)
-      .default(''),
-    lastUpdated: z
-      .string()
-      .nullish()
-      .transform((x) => x ?? undefined)
-      .default(''),
+    title: stringWithDefaultValue(),
+    content: stringWithDefaultValue(),
+    lastUpdated: stringAsCompatibleDate(),
   }),
 });
 
-export type Notes = z.infer<typeof notesSchema>;
+export type Data = z.infer<typeof dataSchema>;
--- app/store.ts
+++ app/store.ts
@@ -1,49 +1,65 @@
-import { type Notes, notesSchema, storageKey } from './schema';
+import { type Data, dataSchema, storageKey } from './schema';
 
 /**
  * Data store, used to store the data in a data store somewhere. The current implementation
  * uses `localStorage` to store all of the data here.
  */
 const dataStore = {
-  storeTitle: (title: string) => {
-    localStorage.setItem(storageKey.TITLE_STORAGE_KEY, title);
+  config: {
+    setFrozen: (frozen: Data['config']['frozen']) => {
+      localStorage.setItem(storageKey.CONFIG_FROZEN_KEY, frozen);
+    },
+    getFrozen: () => {
+      return localStorage.getItem(storageKey.CONFIG_FROZEN_KEY);
+    },
   },
-
-  storeContent: (content: string) => {
-    localStorage.setItem(storageKey.CONTENT_STORAGE_KEY, content);
-  },
-
-  storeLastUpdated: (lastUpdated: string) => {
-    localStorage.setItem(storageKey.LAST_UPDATED_STORAGE_KEY, lastUpdated);
-  },
-
-  getTitle: () => {
-    return localStorage.getItem(storageKey.TITLE_STORAGE_KEY);
-  },
-
-  getContent: () => {
-    return localStorage.getItem(storageKey.CONTENT_STORAGE_KEY);
-  },
-
-  getLastUpdated: () => {
-    return localStorage.getItem(storageKey.LAST_UPDATED_STORAGE_KEY);
+  notes: {
+    title: {
+      storeTitle: (title: string) => {
+        localStorage.setItem(storageKey.TITLE_STORAGE_KEY, title);
+      },
+      getTitle: () => {
+        return localStorage.getItem(storageKey.TITLE_STORAGE_KEY);
+      },
+    },
+    content: {
+      storeContent: (content: string) => {
+        localStorage.setItem(storageKey.CONTENT_STORAGE_KEY, content);
+      },
+      getContent: () => {
+        return localStorage.getItem(storageKey.CONTENT_STORAGE_KEY);
+      },
+    },
+    lastUpdated: {
+      storeLastUpdated: (lastUpdated: string) => {
+        localStorage.setItem(storageKey.LAST_UPDATED_STORAGE_KEY, lastUpdated);
+      },
+      getLastUpdated: () => {
+        return localStorage.getItem(storageKey.LAST_UPDATED_STORAGE_KEY);
+      },
+    },
   },
 };
 
 /**
- * Fetches your own notes from the data store. The return parses
+ * Fetches your own global data from the data store. The return parses
  * it according to the schema to make sure that it's a valid data.
  *
- * @returns A note object.
+ * @returns The data object.
  */
-export const getNotes = (): Notes => {
-  const [title, content, lastUpdated] = [
-    dataStore.getTitle(),
-    dataStore.getContent(),
-    dataStore.getLastUpdated(),
-  ];
+export const getData = () => {
+  const parsedData = dataSchema.parse({
+    config: {
+      frozen: dataStore.config.getFrozen(),
+    },
+    notes: {
+      title: dataStore.notes.title.getTitle(),
+      content: dataStore.notes.content.getContent(),
+      lastUpdated: dataStore.notes.lastUpdated.getLastUpdated(),
+    },
+  });
 
-  return notesSchema.parse({ notes: { title, content, lastUpdated } });
+  return parsedData;
 };
 
 /**
@@ -53,7 +69,7 @@ export const getNotes = (): Notes => {
  * @returns The title.
  */
 export const storeTitle = (title: string) => {
-  dataStore.storeTitle(title);
+  dataStore.notes.title.storeTitle(title);
   return title;
 };
 
@@ -64,7 +80,7 @@ export const storeTitle = (title: string) => {
  * @returns The content of the note.
  */
 export const storeContent = (content: string) => {
-  dataStore.storeContent(content);
+  dataStore.notes.content.storeContent(content);
   return content;
 };
 
@@ -75,6 +91,18 @@ export const storeContent = (content: string) => {
  * @returns The last updated time of the note.
  */
 export const storeLastUpdated = (lastUpdated: string) => {
-  dataStore.storeLastUpdated(lastUpdated);
+  dataStore.notes.lastUpdated.storeLastUpdated(lastUpdated);
   return lastUpdated;
+};
+
+/**
+ * Stores the string representation of a boolean in the data store. Used
+ * to check whether the note is frozen or not.
+ *
+ * @param frozen - A **string** representation of a boolean.
+ * @returns The string representation of the boolean.
+ */
+export const storeFrozen = (frozen: Data['config']['frozen']) => {
+  dataStore.config.setFrozen(frozen);
+  return frozen;
 };
