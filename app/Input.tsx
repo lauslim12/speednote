038472f936f4e9d6fc4d362ff1b0e3a6eda2b6d4
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2023-07-08 22:29:34 +0900

    Add 'id' for Editor's Textarea element

--- app/Input.tsx
+++ app/Input.tsx
@@ -12,16 +12,24 @@ import { type Data } from './schema';
 // We should use `ComponentPropsWithoutRef` to be explicit.
 // Reference: https://react-typescript-cheatsheet.netlify.app/docs/advanced/patterns_by_usecase/#definitely-not-reacthtmlprops-or-reacthtmlattributes
 type InputProps = {
+  id: string;
   type: 'title' | 'content';
   value: string;
   readOnly: Data['config']['frozen'];
   onChange?: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 } & Omit<
   ComponentPropsWithoutRef<'textarea'>,
-  'style' | 'value' | 'onChange' | 'readOnly'
+  'id' | 'style' | 'value' | 'onChange' | 'readOnly'
 >;
 
-const Input = ({ type, value, readOnly, onChange, ...rest }: InputProps) => {
+const Input = ({
+  id,
+  type,
+  value,
+  readOnly,
+  onChange,
+  ...rest
+}: InputProps) => {
   const style = [
     styles.base,
     type === 'title' ? styles.title : styles.content,
@@ -30,6 +38,7 @@ const Input = ({ type, value, readOnly, onChange, ...rest }: InputProps) => {
 
   return (
     <TextareaAutosize
+      id={id}
       className={style}
       onChange={onChange}
       value={value}
