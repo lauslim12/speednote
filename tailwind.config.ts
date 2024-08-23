371694fde1b7c5a2f1aefa47ca89498ccac3b631
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2024-08-24 01:05:20 +0900

    TailwindCSS setup

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import { Config } from 'tailwindcss/types/config';
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', 'index.html'],
+  darkMode: ['selector'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      serif: ['Inter', ...defaultTheme.fontFamily.serif],
+      mono: ['Inter', ...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      colors: {
+        link: '#0984e3',
+        thanks: '#ee5a24',
+        time: '#636e72',
+        frozen: '#808e9b',
+        selection: '#f1c40f',
+        gradient: {
+          start: 'rgba(214, 219, 220, 1)',
+          end: 'rgba(255, 255, 255, 1)',
+        },
+      },
+      boxShadow: {
+        emboss: '0 1rem 4rem rgba(0, 0, 0, 0.25)',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
